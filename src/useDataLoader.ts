a7e6e873fca11f13a829d96575b640590c070e7d
handylee <lihandi@gmail.com>
2019-12-09 16:12:26 +0800

    feat: :sparkles: 增加 useSwr hooks

--- src/useDataLoader.ts
+++ src/useDataLoader.ts
@@ -1,4 +1,4 @@
-import { useState, useReducer, useEffect, Dispatch } from 'react'
+import { useState, useReducer, useEffect, Dispatch, useRef } from 'react'
 
 interface State<T = any> {
   data: T
@@ -55,16 +55,65 @@ function reducer<T>(state: State<T>, action: Action): State<T> {
 type Reverse<T> = (...args: any) => Promise<T>
 type ReturnType<T> = State<T> & { retry: () => void; update: (data: Partial<T>) => void }
 
-function useDataLoader<T>(getData: Reverse<T>, ...args: any): ReturnType<T> {
+
+const __cache = new Map()
+function cacheGet(key: string) {
+  if (!key) return undefined
+  return __cache.get(key) || { data: undefined }
+}
+function cacheSet(key: string, value: any) {
+  return __cache.set(key, value)
+}
+function cacheClear(key: string) {
+  return __cache.delete(key)
+}
+
+type PropertyFunction<T> = () => T
+type KeyArgs = string | PropertyFunction<string>
+
+const getKey = (_key: KeyArgs): string => {
+  let key
+  if (typeof _key === 'function') {
+    try {
+      key = _key()
+    } catch (err) {
+      key = ''
+    }
+  } else {
+    // 默认一个随机串，否则 useDataLoader 传 空 key 时， 会不请求数据
+    key = String(_key || Date.now() + '-' + Math.random())
+  }
+  return key
+}
+
+export function useSWR<T>(_key: KeyArgs, getData: Reverse<T>, ...args: any): ReturnType<T> {
+  const useCache = !!_key
+  const key = getKey(_key)
+  const keyRef = useRef(key)
+
   const [nonce, setNonce] = useState(Date.now())
   const [state, dispatch] = useReducer(reducer, {
-    data: undefined,
+    data: useCache ? (cacheGet(key) ? cacheGet(key).data : undefined) : undefined,
     error: undefined,
     loading: true,
     loaded: false
   }) as [State<T>, Dispatch<Action>]
 
+
+  const retry = () => setNonce(Date.now())
+
+  const update = (data: Partial<T>) => dispatch({ type: 'update', payload: { data } })
+
   useEffect(() => {
+    if (!useCache) return
+    if (key !== keyRef.current) {
+      cacheClear(keyRef.current)
+      keyRef.current = key
+    }
+  }, [key])
+
+  useEffect(() => {
+    if (!key) return
     let cancel = false
     dispatch({ type: 'get' })
     getData(...args)
@@ -73,6 +122,7 @@ function useDataLoader<T>(getData: Reverse<T>, ...args: any): ReturnType<T> {
         if (data.errors && data.errors.length) {
           return dispatch({ type: 'error', payload: { error: data.errors[0] } })
         }
+        useCache && cacheSet(key, { data, retry, update })
         dispatch({ type: 'success', payload: { data } })
       })
       .catch((error: any) => {
@@ -83,17 +133,18 @@ function useDataLoader<T>(getData: Reverse<T>, ...args: any): ReturnType<T> {
     return () => {
       cancel = true
     }
-  }, [nonce, ...args])
+  }, [nonce, key, ...args])
 
-  const retry = () => setNonce(Date.now())
-
-  const update = (data: Partial<T>) => dispatch({ type: 'update', payload: { data } })
 
   return {
     ...state,
     retry,
     update
   }
+}
+
+function useDataLoader<T>(getData: Reverse<T>, ...args: any): ReturnType<T> {
+  return useSWR<T>('', getData, ...args)
 }
 
 export default useDataLoader
